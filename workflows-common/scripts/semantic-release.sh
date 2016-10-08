1dbe8dbd385f4a17e91212efc0fa2b2bfc056a68
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2016-10-08 18:00:37 +0200

    Fix semantic releasing

---
+++ workflows-common/scripts/semantic-release.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+cd project
+
+if [ -n "${GIT_USERNAME}" ]; then
+  git config --global user.name ${GIT_USERNAME}
+fi
+
+if [ -n "${GIT_EMAIL}" ]; then
+  git config --global user.email ${GIT_EMAIL}
+fi
+
+semantic-release pre && npm publish && semantic-release post
