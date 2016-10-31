a6f0296156c2d617b7a6a51c3bc8c8ec70064625
Tibor Ujlaky <tujlaky@gmail.com>
2016-10-31 19:59:45 +0100

    optional vnc in protractor

--- protractor/protractor.sh
+++ protractor/protractor.sh
@@ -2,5 +2,9 @@
 
 Xvfb -screen 0 1280x1024x24 -ac &
 export DISPLAY=:0 ratpoison
-x11vnc -nopw -display :0 -forever &
+
+if [[ $DEBUG ]]; then
+  x11vnc -nopw -display :0 -forever &
+fi
+
 protractor $@
