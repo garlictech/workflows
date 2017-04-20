1bea0426d4846d5c93829446aa2a394af04f9a66
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2017-04-20 17:20:38 +0200

    fix: minor

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,4 +6,5 @@ IMAGE_NAME=${1}
 
 docker build -t ${DOCKER_REPO_HOST}/${IMAGE_NAME} .
 # docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
+# docker tag ${DOCKER_REPO_HOST}/${IMAGE_NAME} ${DOCKER_REPO_HOST}/${IMAGE_NAME}:v1.18.4
 # docker push ${DOCKER_REPO_HOST}/${IMAGE_NAME}
