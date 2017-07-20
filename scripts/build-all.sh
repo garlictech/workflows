d70bc76348dda132aa42a818129492fee91f07b7
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2017-07-20 10:57:40 +0200

    fix: Update to npm 5.3.0 && support gulp hooks

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -1,4 +1,4 @@
-WORKFLOWS="base-image angular2-common angular2-webapp angular2-module workflows-base workflows-common workflows-library workflows-server server-common"
+WORKFLOWS="base-image workflows-base workflows-common angular2-common angular2-webapp angular2-module workflows-library workflows-server server-common"
 # WORKFLOWS="base-image angular2-app-server angular2-common angular2-webapp angular2-module workflows-base workflows-common workflows-library workflows-server workflows-loopback-server server-common server-loopback protractor"
 
 for i in $WORKFLOWS; do
