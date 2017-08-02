1ef5a7494037cb560d962b7894f631bab2423e3c
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2017-08-02 20:18:06 +0200

    fix: Upgrade to angular 4

--- angular2-common/scripts/server-dev.sh
+++ angular2-common/scripts/server-dev.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-webpack-dev-server --config config/webpack.dev.js --open --progress --profile --watch --port 8081 --host 0.0.0.0 --content-base project/src/ $@
+webpack-dev-server --config config/webpack.dev.js --progress --profile --watch --port 8081 --host 0.0.0.0 --content-base project/src/ $@
--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ SCOPE=garlictech2
 DOCKER_REPO_HOST=${SCOPE}
 IMAGE_NAME=${1}
 
-docker build --pull -t ${DOCKER_REPO_HOST}/${IMAGE_NAME} .
+docker build -t ${DOCKER_REPO_HOST}/${IMAGE_NAME} .
 # docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
 # docker tag ${DOCKER_REPO_HOST}/${IMAGE_NAME} ${DOCKER_REPO_HOST}/${IMAGE_NAME}:v1.18.4
 # docker push ${DOCKER_REPO_HOST}/${IMAGE_NAME}
