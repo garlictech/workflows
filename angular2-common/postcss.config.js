835f0c3778f2d22fbd4c1058236818a2fb312a2a
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2017-09-18 12:01:06 +0200

    fix: Enable postcss in source maps

--- angular2-common/postcss.config.js
+++ angular2-common/postcss.config.js
@@ -2,9 +2,9 @@
 
 module.exports = {
     plugins: [
-        require('postcss-smart-import')(),
-        require('postcss-cssnext')(),
-        require('postcss-apply')(),
-        require('postcss-responsive-type')()
+        require('postcss-smart-import')({ sourceMap: true }),
+        require('postcss-cssnext')({ sourceMap: true }),
+        require('postcss-apply')({ sourceMap: true }),
+        require('postcss-responsive-type')({ sourceMap: true })
     ]
 };
