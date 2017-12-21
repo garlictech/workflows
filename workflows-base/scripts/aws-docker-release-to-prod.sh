bfc59525035080fedd2e06989a092a45e66d11fa
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2017-12-21 19:17:59 +0100

    fix: aws deploy script: dev/prod builds depending on travis branch

--- workflows-base/scripts/aws-docker-release-to-prod.sh
+++ workflows-base/scripts/aws-docker-release-to-prod.sh
@@ -5,15 +5,18 @@ if [ $TRAVIS_BRANCH = "master" ]; then
   export DOCKER_IMAGE_SCOPE=prod
 fi
 
+DOCKER_IMAGE=${DOCKER_REGISTRY}/${PROJECT}-${DOCKER_IMAGE_SCOPE}
+DOCKER_TAG=${DOCKER_IMAGE}:$(scripts/get-docker-tag.sh)
+
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > $HOME/.npmrc
 S3_BUCKET=${PROJECT_SCOPE}-deployment
-RELEASE_FILE=${PROJECT}-${DOCKER_IMAGE_SCOPE}_deploy.json
+RELEASE_FILE_CC=${PROJECT}-${DOCKER_IMAGE_SCOPE}-deploy.json
+RELEASE_FILE=$(echo ${RELEASE_FILE_CC} | sed -r 's/(^|-)(\w)/\U\2/g')
 RELEASE_FILE_ZIP=${RELEASE_FILE}.zip
 
 aws configure set default.s3.signature_version s3v4
 cd /app/project
-DOCKER_TAG=$(/app/scripts/get-docker-tag.sh)
-echo "{\"Tag\": \"${DOCKER_TAG}\"}" > ${RELEASE_FILE}
+echo "{\"name\": \"${PROJECT}-${DOCKER_IMAGE_SCOPE}\"}{\"imageUri\": \"${DOCKER_TAG}\"}" > ${RELEASE_FILE}
 zip ${RELEASE_FILE_ZIP} ${RELEASE_FILE}
 aws s3 cp ${RELEASE_FILE_ZIP} s3://${S3_BUCKET}/${PROJECT}/${RELEASE_FILE_ZIP}
 
