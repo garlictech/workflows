5938cc43c27ce81c057df139cfda42c11ea9e7d1
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2018-03-29 20:20:02 +0200

    fix: Debugger support for the server side jasmine tests, resolves #25

---
+++ workflows-common/scripts/systemtest-debug.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+gulp build --gulpfile gulpfile-systemtest.coffee
+echo "*******************************************************************************************************"
+echo "* See the instructions at https://github.com/garlictech/workflows/blob/master/README.md#debug-execution"
+echo "*******************************************************************************************************"
+node --inspect-brk=0.0.0.0:9229 ./node_modules/.bin/gulp systemtest --gulpfile gulpfile-systemtest.coffee
