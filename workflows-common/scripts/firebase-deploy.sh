d6f9ea5ab7f04ceb88dd03befa6609983d3bb9cc
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2018-11-12 15:54:18 +0100

    fix: firebase deployment fix

--- workflows-common/scripts/firebase-deploy.sh
+++ workflows-common/scripts/firebase-deploy.sh
@@ -1,14 +1,3 @@
 #!/usr/bin/env bash
 cd /app/project
-
-firebase use $1
-firebase deploy --token "$FIREBASE_TOKEN" --only hosting:$1
-
-firebase use docs
-firebase deploy --token "$FIREBASE_TOKEN" --only hosting:docs
-
-firebase use coverage
-firebase deploy --token "$FIREBASE_TOKEN" --only hosting:coverage
-
-firebase use e2e
-firebase deploy --token "$FIREBASE_TOKEN" --only hosting:e2e
+firebase deploy --token "$FIREBASE_TOKEN"
