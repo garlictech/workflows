85ccea2e86691ff0198ac053c1c40e4e75ec03d7
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2019-03-13 10:12:23 +0100

    feat: unit/system test execution support

--- workflows-angular/jest.config.unittest.js
+++ workflows-angular/jest.config.unittest.js
@@ -1,11 +1,15 @@
-// Don't use this file as jest config, use the app-level config file in src. That file must import this one and re-export
-// it with or without modifications.
-// That file contains the workflow level global settigns, so if you overwrite this one, the workflow test runs may
-// return different result.
-
-var globalConf = require('./jest.config');
+'esversion: 6';
+var globalConf = require('../jest.config.unittest');
+var localConf = require('./jest.config');
 
 module.exports = {
     ...globalConf,
-    testPathIgnorePatterns: ['/node_modules/', '/artifacts/', '<rootDir>/.git/', '<rootDir>/test/']
+    ...localConf,
+    testPathIgnorePatterns: [
+        '/node_modules/',
+        '/artifacts/',
+        '<rootDir>/.git/',
+        '<rootDir>/test/',
+        '<rootDir>/src/common/test/integration'
+    ]
 };
