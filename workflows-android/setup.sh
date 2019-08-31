66c4c804e5b19cdec7d80a2018ea36e29b23543d
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2019-08-31 22:12:59 +0200

    release: node 12 upgrade

--- workflows-android/setup.sh
+++ workflows-android/setup.sh
@@ -5,7 +5,7 @@ apt-get install -y --no-install-recommends apt-transport-https vim build-essenti
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
+curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
 bash nodesource_setup.sh
 rm nodesource_setup.sh
 apt-get install -y nodejs yarn
